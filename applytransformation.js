a2208511f0f1e0232b54234f22b714febbd29f8a
NishantGangwar492 <vp5391849@gmail.com>
2023-12-15 02:38:07 +0530

    fifth day

---
+++ applytransformation.js
@@ -0,0 +1,25 @@
+/***
+ * Given an integer array arr and a mapping function fn, 
+ * return a new array with a transformation applied to 
+ * each element.
+
+The returned array should be created such that 
+returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+ */
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let array = []
+    arr.forEach((item,i)=>{
+        array.push(fn(item,i))
+    })
+    return array
+ };
+ 
