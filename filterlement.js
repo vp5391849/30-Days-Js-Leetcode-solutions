918483227e643a166097097c466285400f0268f7
NishantGangwar492 <vp5391849@gmail.com>
2023-12-16 00:40:39 +0530

    fifth day

---
+++ filterlement.js
@@ -0,0 +1,33 @@
+/***
+ * 
+Given an integer array arr and a filtering function fn, 
+return a filtered array filteredArr.
+
+The fn function takes one or two arguments:
+
+arr[i] - number from the arr
+i - index of arr[i]
+filteredArr should only contain the elements from the 
+arr for which the expression fn(arr[i], i) evaluates 
+to a truthy value. A truthy value is a value where 
+Boolean(value) returns true.
+
+Please solve it without the built-in Array.filter method.
+ */
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+
+    array2 = []
+    arr.forEach((item,i)=>{
+        if(fn(item,i)){
+           array2.push(item)
+        }
+    })
+        return array2
+    }
+     
